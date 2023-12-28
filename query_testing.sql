4e49954d28a8c0dab0baf8c3c59894feba2d1f44
paulhji <thisispaulji@gmail.com>
2023-12-28 09:44:16 -0800

    Query testing sql file

---
+++ query_testing.sql
@@ -0,0 +1,13 @@
+-- Query testing
+
+-- Querying from campaign table
+SELECT * FROM campaign;
+
+-- Querying from category table
+SELECT * FROM category;
+
+-- Querying from contacts table
+SELECT * FROM contacts;
+
+-- Querying from subcategory table
+SELECT * FROM subcategory;
